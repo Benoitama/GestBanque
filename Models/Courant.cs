3bc988e7e0f86008e6244c83ae8a06c03817f94e
Benoit <schmitz.benoit.pro@outlook.com>
2024-03-25 15:14:21 +0100

    correction - ctrl c v Thierry

--- Models/Courant.cs
+++ Models/Courant.cs
@@ -1,48 +1,8 @@
-﻿using System.Security.Cryptography;
+﻿namespace Models;
 
-namespace Models;
-
-
-public class Courant
+public class Courant : Compte
 {
-    private string _numero;
-    private double _solde;
     private double _ligneDeCredit;
-    private Personne _titulaire;
-    
-
-
-    public static double operator +(double amount, Courant s2)  //surcharge d'operateur
-    {
-
-        return (amount < 0 ? 0 : amount) + (s2.Solde < 0 ? 0 : s2.Solde);
-    }
-
-    public string Numero
-    {
-        get
-        {
-            return _numero;
-        }
-
-        set
-        {
-            _numero = value;
-        }
-    }
-
-    public double Solde
-    {
-        get
-        {
-            return _solde;
-        }
-
-        private set
-        {
-            _solde = value;
-        }
-    }
 
     public double LigneDeCredit
     {
@@ -53,58 +13,17 @@ public class Courant
 
         set
         {
-            if(value < 0)
+            if (value < 0)
             {
-                Console.WriteLine("La valeur doit être strictement positive");
+                Console.WriteLine("La ligne de crédit est strictement positive..."); // => Erreur : Exception
                 return;
             }
             _ligneDeCredit = value;
         }
     }
 
-    public Personne Titulaire
+    public override void Retrait(double montant)
     {
-        get
-        {
-            return _titulaire;
-        }
-
-        set
-        {
-            _titulaire = value;
-        }
-    }
-
-    public void Retrait(double Montant)
-    {
-        if(Montant <= 0)
-        {
-            Console.WriteLine("Impossible de retirer un montant négatif");
-            return;
-        }
-            
-        if(Solde - Montant < -LigneDeCredit)
-        {
-        Console.WriteLine("Vous n'avez pas suffisamment d'argent");
-        return;
-        }
-            
-        Solde -= Montant;
-        Console.WriteLine("Voici votre argent");
-            
-    }
-
-    public void Depot(double Montant)
-    {
-    if (Montant <= 0)
-    {
-        Console.WriteLine("Impossible de déposer une valeur négative");
-        return;
-    }
-        
-        Solde += Montant;
-        Console.WriteLine("L'argent à bien été ajouté à votre compte.");
-                
+        Retrait(montant, LigneDeCredit);
     }
 }
-
